110422e712c3330db4a548dfab1986f9f8ba746d
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2025-10-29 18:05:04 -0500

    implemented spring security part 1

--- src/main/java/com/springboot/backend/salazar/usersbackend/users_backend/entities/User.java
+++ src/main/java/com/springboot/backend/salazar/usersbackend/users_backend/entities/User.java
@@ -1,10 +1,21 @@
 package com.springboot.backend.salazar.usersbackend.users_backend.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import static jakarta.persistence.GenerationType.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -32,6 +43,21 @@ public class User {
 
     @NotEmpty
     private String password;
+
+    @JsonIgnoreProperties({"handler, hibernateLazyInitializer"})
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = {@JoinColumn(name ="user_id")},
+        inverseJoinColumns = @JoinColumn(name="role_id"),
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id, role_id"})}
+    )
+    private List<Role> roles;
+
+
+    public User(List<Role> roles) {
+        this.roles = new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -74,4 +100,14 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    
 }
--- src/main/java/com/springboot/backend/salazar/usersbackend/users_backend/repositories/UserRepository.java
+++ src/main/java/com/springboot/backend/salazar/usersbackend/users_backend/repositories/UserRepository.java
@@ -1,13 +1,18 @@
 package com.springboot.backend.salazar.usersbackend.users_backend.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.springboot.backend.salazar.usersbackend.users_backend.entities.User;
 
+
 public interface UserRepository extends CrudRepository<User, Long> {
 
     Page<User> findAll(Pageable pageable);
+
+    Optional<User>  findByUsername(String username);
 
 }
