03bcefc0feadaae725517e7966962ddef7c04f1c
Juan Fernando Salazar Serrano <juanfernandosalazarserrano@gmail.com>
2025-11-01 20:52:26 -0500

    jwt auth part 1

---
+++ src/main/java/com/springboot/backend/salazar/usersbackend/users_backend/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.springboot.backend.salazar.usersbackend.users_backend.auth.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.backend.salazar.usersbackend.users_backend.entities.User;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+
+    private AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+                
+                String username = null;
+                String password = null;
+
+                try {
+                    User user = new ObjectMapper().readValue(request.getInputStream(),User.class);
+                    username = user.getUsername();
+                    password = user.getPassword();                   
+                } catch (StreamReadException e) {
+                    e.printStackTrace();
+                } catch (DatabindException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    username, password);
+
+            return this.authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
+}
